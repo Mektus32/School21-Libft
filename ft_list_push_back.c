39045004ddc46d98e7d8d1212183ca0beabec83c
Otho Jessi <ojessi@at-p7.21-school.ru>
2019-04-16 18:54:57 +0300

    modif push_back

--- ft_list_push_back.c
+++ ft_list_push_back.c
@@ -12,7 +12,7 @@
 
 #include "libft.h"
 
-void	ft_list_push_back(t_list **begin_list, void *str, int elem)
+t_list	*ft_list_push_back(t_list **begin_list, void *str, int elem)
 {
 	t_list	*list;
 
@@ -27,4 +27,5 @@ void	ft_list_push_back(t_list **begin_list, void *str, int elem)
 			list = list->next;
 		list->next = ft_list_add(str, elem);
 	}
+	return (list);
 }
